a540b9cadc65d5b297bc325f6cda972a9d1d9623
Sergey <grishy.ru@yandex.ru>
2018-03-21 23:20:30 +0300

    simple lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,16 +1,12 @@
 import re
 
 token_pattern = r"""
-(?P<identifier>[a-zA-Z_][a-zA-Z0-9_]*)
-|(?P<integer>[0-9]+)
-|(?P<dot>\.)
-|(?P<open_variable>[$][{])
-|(?P<open_curly>[{])
-|(?P<close_curly>[}])
+(?P<int>[0-9]+)
+|(?P<plus>[+])
+|(?P<open_round_brackets>[(])
+|(?P<close_round_brackets>[)])
 |(?P<newline>\n)
-|(?P<whitespace>\s+)
-|(?P<equals>[=])
-|(?P<slash>[/])
+|(?P<whitespace>\s)
 """
 
 token_re = re.compile(token_pattern, re.VERBOSE)
@@ -30,19 +26,9 @@ def tokenize(text):
         raise TokenizerException('tokenizer stopped at pos %r of %r' % (
             pos, len(text)))
 
-stuff = r'property.${general.name}.ip = ${general.ip}'
 stuff2 = r'''
-general {
-  name = myname
-  ip = 127.0.0.1
-}
+123 + 21332 +  (2345345)  + 1
 '''
 
-print(' stuff '.center(60, '='))
-for tok in tokenize(stuff):
-    print(tok)
-
-
-print(' stuff2 '.center(60, '='))
 for tok in tokenize(stuff2):
     print(tok)
