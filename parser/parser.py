6c60050728a0053d16c721ee0ef928e2d911f35c
Sergey <grishy.ru@yandex.ru>
2018-03-22 23:26:37 +0300

    parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,69 +1,38 @@
-from __future__ import unicode_literals
-from parglare import Grammar, Parser
-from parglare.actions import pass_inner, pass_single
+from parglare import Parser, Grammar
 
-grammar = """
-Calc: Assignments E;
-Assignments: Assignment | Assignments Assignment | EMPTY;
-Assignment: VariableName "=" Number;
-E: E "+" E {left, 1}
- | E "-" E {left, 1}
- | E "*" E {left, 2}
- | E "/" E {left, 2}
- | "(" E ")"
- | VariableRef
- | Number
-;
-VariableRef: VariableName;
-VariableName: /[a-zA-Z_][_a-zA-Z0-9]*/;
-Number: /\d+(\.\d+)?/;
+grammar = r"""
+E: E '+' E  {left, 1}
+| E '-' E  {left, 1}
+| E '*' E  {left, 2}
+| E '/' E  {left, 2}
+| E '^' E  {right, 3}
+| '(' E ')'
+| number;
+number: /\d+(\.\d+)?/;
 """
 
+def ttt(_, value):
+    print(value)
+    return float(value)
 
-# Semantic Actions
-def act_assignment(context, nodes):
-    """Semantic action for variable assignment."""
-
-    name = nodes[0]
-    number = nodes[2]
-
-    if not hasattr(context, 'variables'):
-        context.variables = {}
-
-    context.variables[name] = number
-
+def pl(_, nodes):
+    print(nodes)
+    return nodes[0] + nodes[2]
 
 actions = {
-    "Calc": pass_inner,
-    "Assignment": act_assignment,
-    "E": [lambda _, nodes: nodes[0] + nodes[2],
+    "E": [pl,
           lambda _, nodes: nodes[0] - nodes[2],
           lambda _, nodes: nodes[0] * nodes[2],
           lambda _, nodes: nodes[0] / nodes[2],
-          pass_inner,
-          pass_single,
-          pass_single],
-    "Number": lambda _, value: float(value),
-    "VariableRef": lambda context, nodes: context.variables[nodes[0]],
+          lambda _, nodes: nodes[0] ** nodes[2],
+          lambda _, nodes: nodes[1],
+          lambda _, nodes: nodes[0]],
+    "number": ttt,
 }
 
+g = Grammar.from_string(grammar)
+parser = Parser(g, debug=False, actions=actions)
 
-def main(debug=False):
-    g = Grammar.from_string(grammar, debug=debug, debug_colors=True)
-    parser = Parser(g, actions=actions, debug=debug, debug_colors=True)
+result = parser.parse("34 + 4.6 / 2 * 4^2^2 + 78")
 
-    input_str = """
-    a = 5
-    b = 10
-    a + 56.4 / 3 * 5 - b + 8 * 3
-    """
-
-    res = parser.parse(input_str)
-
-    assert res == 5. + 56.4 / 3 * 5 - 10 + 8 * 3
-    print("Input:\n", input_str)
-    print("Result = ", res)
-
-
-if __name__ == "__main__":
-    main(debug=True)
+print("Result = ", result)
