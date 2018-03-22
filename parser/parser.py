768db44181c7f396cb28b9998b28a4a9025b7e74
Sergey <grishy.ru@yandex.ru>
2018-03-22 23:05:28 +0300

    calc

--- parser/parser.py
+++ parser/parser.py
@@ -1,30 +1,69 @@
-from parglare import Parser, Grammar
+from __future__ import unicode_literals
+from parglare import Grammar, Parser
+from parglare.actions import pass_inner, pass_single
 
-grammar = r"""
-E: E '+' E  {left, 1}
-| E '-' E  {left, 1}
-| E '*' E  {left, 2}
-| E '/' E  {left, 2}
-| E '^' E  {right, 3}
-| '(' E ')'
-| number;
-number: /\d+(\.\d+)?/;
+grammar = """
+Calc: Assignments E;
+Assignments: Assignment | Assignments Assignment | EMPTY;
+Assignment: VariableName "=" Number;
+E: E "+" E {left, 1}
+ | E "-" E {left, 1}
+ | E "*" E {left, 2}
+ | E "/" E {left, 2}
+ | "(" E ")"
+ | VariableRef
+ | Number
+;
+VariableRef: VariableName;
+VariableName: /[a-zA-Z_][_a-zA-Z0-9]*/;
+Number: /\d+(\.\d+)?/;
 """
 
+
+# Semantic Actions
+def act_assignment(context, nodes):
+    """Semantic action for variable assignment."""
+
+    name = nodes[0]
+    number = nodes[2]
+
+    if not hasattr(context, 'variables'):
+        context.variables = {}
+
+    context.variables[name] = number
+
+
 actions = {
+    "Calc": pass_inner,
+    "Assignment": act_assignment,
     "E": [lambda _, nodes: nodes[0] + nodes[2],
           lambda _, nodes: nodes[0] - nodes[2],
           lambda _, nodes: nodes[0] * nodes[2],
           lambda _, nodes: nodes[0] / nodes[2],
-          lambda _, nodes: nodes[0] ** nodes[2],
-          lambda _, nodes: nodes[1],
-          lambda _, nodes: nodes[0]],
-    "number": lambda _, value: float(value),
+          pass_inner,
+          pass_single,
+          pass_single],
+    "Number": lambda _, value: float(value),
+    "VariableRef": lambda context, nodes: context.variables[nodes[0]],
 }
 
-g = Grammar.from_string(grammar)
-parser = Parser(g, debug=True, actions=actions)
 
-result = parser.parse("34 + 4.6 / 2 * 4^2^2 + 78")
+def main(debug=False):
+    g = Grammar.from_string(grammar, debug=debug, debug_colors=True)
+    parser = Parser(g, actions=actions, debug=debug, debug_colors=True)
 
-print("Result = ", result)
+    input_str = """
+    a = 5
+    b = 10
+    a + 56.4 / 3 * 5 - b + 8 * 3
+    """
+
+    res = parser.parse(input_str)
+
+    assert res == 5. + 56.4 / 3 * 5 - 10 + 8 * 3
+    print("Input:\n", input_str)
+    print("Result = ", res)
+
+
+if __name__ == "__main__":
+    main(debug=True)
