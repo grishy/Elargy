b6e40bbdd2703a601e5836ceaf6007d6a48b78b6
Grishy <grishy.ru@yandex.ru>
2018-03-25 20:49:13 +0300

    count linex

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,18 +1,29 @@
 import re
  
-def identifier(scanner, token): return "IDENT", token
-def operator(scanner, token):   return "OPERATOR", token
-def digit(scanner, token):      return "DIGIT", token
-def end_stmnt(scanner, token):  return "END_STATEMENT"
+line = 1
+
+def identifier(scanner, token): return "IDENT", token, scanner.match.regs[0], line
+def operator(scanner, token):   return "OPERATOR", token, scanner.match.regs[0], line
+def digit(scanner, token):      return "DIGIT", token, scanner.match.regs[0], line
+def end_stmnt(scanner, token):  return "END_STATEMENT", scanner.match.regs[0], line
+def next_line(scanner, token):  
+    global line 
+    line = line + 1
+    return "NEXT_LINE", scanner.match.regs[0], line
  
 scanner = re.Scanner([
     (r"[a-zA-Z_]\w*", identifier),
     (r"\+|\-|\\|\*|\=", operator),
     (r"[0-9]+(\.[0-9]+)?", digit),
     (r";", end_stmnt),
+    (r"\n", next_line),
     (r"\s+", None),
     ])
  
-tokens, remainder = scanner.scan("foo = 5 * 30; bar = bar - 60;")
+tokens, remainder = scanner.scan("""
+foo = 5 * 30; bar = bar - 60;
+sdf
+sdfg
+""")
 for token in tokens:
     print(token)
