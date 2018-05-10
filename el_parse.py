611739e92c48dd0caf793fd18a805c8a8be819f3
Grishy <grishy.ru@yandex.ru>
2018-05-10 22:34:39 +0300

    первое и второе правило

--- el_parse.py
+++ el_parse.py
@@ -5,6 +5,12 @@ class ElRuleSymbol:
         self.left = set()
         self.right = set()
 
+    def setLeft(self, idx):
+        self.left.add(idx)
+
+    def setRight(self, idx):
+        self.right.add(idx)
+
 
 class ElRule:
     def __init__(self, text):
@@ -22,38 +28,55 @@ class ElParser:
         self.rules_text = open(grammarFile, "r").readlines()
         self.rules = [ElRule(t) for t in self.rules_text]
 
-        # Set "Not-Term" or "Term" in ElRuleSymbol
         self.setType()
 
-        self.SetIndex()
+        self.setIndex()
         print(self.rules)
 
     def setType(self):
+        """Расстановка символу их типа: Терменал/Не терминал"""
         not_ter = {r.left_side for r in self.rules}
-
         for r in self.rules:
             for s in r.right_side:
                 s.type = "Not-Term" if s.text in not_ter else s.type
 
-    def SetIndex(self):
-        idx = 1
+    def setIndex(self):
+        """
+        Предварительная расстановка индексов символам. Без использования 3 правила.
+        """
+        IDX = 1
         for r in self.rules:
-            r.setFirst(idx)
+            for i in range(len(r.right_side)):
+                sym = r.right_side[i]
+                try:
+                    sym_next = r.right_side[i + 1]
+                except (IndexError, ValueError):
+                    sym_next = None
+
+                # Если слева пусто -> ставим этому символу его и во всех правилах
+                if len(sym.left) == 0:
+                    sym.setLeft(IDX)
+                    self.setLeftAll(sym, IDX)
+                    IDX += 1
+
+                # Ставим спарва указанный увеличенный символ
+                sym.setRight(IDX)
+                # У следующего символа тоже, на следующем этапе будет оптимизировано
+                if sym_next is not None:
+                    sym_next.setLeft(IDX)
+
+                IDX += 1
+
+    def setLeftAll(self, sym, idx):
+        """
+        У всех парвил, где слева идёт указанный симовол, ставил справа у первого сивола указанный индекс.
+        Для первого симовла выполняет эту функцию с таким же параметром.
+        """
+        for r in self.findRulesLeft(sym):
             first_sym = r.right_side[0]
-            self.setFirstIndex(first_sym.text, idx)
-            idx += 1
-            first_sym.right.add(idx)
-            
+            first_sym.setLeft(idx)
+            self.setLeftAll(first_sym, idx)
 
-    def setFirstIndex(self, sym, idx):
-        rules = self.findRuleLeft(sym)
-
-        for r in rules:
-            first_right =r.right_side[0]
-            r.setFirst(idx)
-            self.setFirstIndex(first_right.text, idx)
-        
-        
-
-    def findRuleLeft(self, text):
-        return [r for r in self.rules if r.left_side == text]
+    def findRulesLeft(self, sym):
+        """Найти все паравила, где слева идет указанный символ"""
+        return [r for r in self.rules if r.left_side == sym.text]
