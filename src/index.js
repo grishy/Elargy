a2a698f62bc98c57d16edcedd5d201bffb3a2bf6
kirill.larionov1998 <kirill.larionov1998@gmail.com>
2018-11-07 19:20:27 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -21,3 +21,35 @@ function makeParser(){
     var parseTree = prsr.parseString(prsr.lexer($("#input-text").val().trim()));
     prsr.parseTreeToHTML(prsr.parseTree);
 }
+
+function example1(){
+    $(".grammar-text").val(`S => A
+S => D
+A => a b
+A => a c
+A => A b
+D => c D
+D => b`);
+    $(".terminals-text").val(`a|b|c`);
+    $("#input-text").val(`a b b`);
+}
+
+function example2(){
+    $(".grammar-text").val(`S=>a A B e
+A=>A b c
+A=>b
+B=>d`);
+    $(".terminals-text").val(`a|b|c|d|e`);
+    $("#input-text").val(`a b b c d e`);
+}
+
+function example3(){
+    $(".grammar-text").val(`S => E
+E => T PLUS E
+E => T
+T => F MULT T
+T => F
+F => INTEGER`);
+    $(".terminals-text").val(`INTEGER|PLUS|MULT`);
+    $("#input-text").val(`INTEGER PLUS INTEGER MULT INTEGER`);
+}
